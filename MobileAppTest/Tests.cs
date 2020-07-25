b572561e780668712b561f765d81a0292a2c1faf
Vitaly Zubkov <zubkov.v.o@gmail.com>
2020-07-25 16:25:13 +0300

    Created automation tests

---
+++ MobileAppTest/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace MobileAppTest
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+        public string _path = @"D:\Logs\test.apk";
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform, _path);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            app.Repl();
+        }
+    }
+}
